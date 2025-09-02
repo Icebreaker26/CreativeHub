3dd69f425ddd30a122fba926fc9099d21caca426
Icebreaker26 <alejandro.torres0826@gmail.com>
2025-09-02 15:07:08 -0500

    0.1.5

--- src/App.js
+++ src/App.js
@@ -45,11 +45,11 @@ function App() {
       <div className="section section-1">
         <div className="left">
 
-          <h3 className="project-title">The Linkin' Tool</h3>
+          <h3 className="project-title">Cooperativa Progresemos</h3>
           <div className="divider"></div>
           
-          <p className="project-subtitle">Software para la planificación de enlaces PTP</p>
-          <p className="project-tech">React JS + HTML5 + CSS + API + Ingeniería Telecomunicaciones</p>
+          <p className="project-subtitle">WordPress y Análisis de Datos</p>
+          <p className="project-tech">WordPress + Elementor + Excel + Jupyter Notebook</p>
 
 
         </div>
@@ -64,17 +64,17 @@ function App() {
 
       <div className="section section-2">
         <div className="left">
-          <h3 className="project-title">The Linkin' Tool</h3>
+          <h3 className="project-title">Data Analyst</h3>
           <div className="divider"></div>
-          <p className="project-subtitle">Software para la planificación de enlaces PTP</p>
-          <p className="project-tech">React JS + HTML5 + CSS + API + Ingeniería Telecomunicaciones</p>
+          <p className="project-subtitle">Exploración de datos históricos sobre la penetración de Internet en Colombia entre 2016 y 2023</p>
+          <p className="project-tech">Python + Pandas + Jupyter Notebook + Matplotlib</p>
         </div>
         <div className="right">
           
-          <h3 className="project-title">The Linkin' Tool</h3>
+          <h3 className="project-title">Pruebas de Concepto</h3>
           <div className="divider"></div>
-          <p className="project-subtitle">Software para la planificación de enlaces PTP</p>
-          <p className="project-tech">React JS + HTML5 + CSS + API + Ingeniería Telecomunicaciones</p>
+          <p className="project-subtitle">Una colección de experimentos y proyectos que exploran diversas áreas.</p>
+          <p className="project-tech">React JS + HTML5 + CSS + API + Python + Jupyter Notebook </p>
 
 
         </div>
@@ -82,19 +82,19 @@ function App() {
 
       <div className="section section-3">
         <div className="left">
-       <h3 className="project-title">The Linkin' Tool</h3>
+       <h3 className="project-title">Pereira En Guerra</h3>
           <div className="divider"></div>
-          <p className="project-subtitle">Software para la planificación de enlaces PTP</p>
-          <p className="project-tech">React JS + HTML5 + CSS + API + Ingeniería Telecomunicaciones</p>
+          <p className="project-subtitle">Demo de videojuego 2D estilo Run and Gun ambientado en Pereira post apocalíptica. </p>
+          <p className="project-tech">Unity (EN DESARROLLO)</p>
 
 
           </div>
         <div className="right">
 
-         <h3 className="project-title">The Linkin' Tool</h3>
+         <h3 className="project-title">Optimización de Producción Ganadera con ML y Visión por Computador</h3>
           <div className="divider"></div>
-          <p className="project-subtitle">Software para la planificación de enlaces PTP</p>
-          <p className="project-tech">React JS + HTML5 + CSS + API + Ingeniería Telecomunicaciones</p>
+          <p className="project-subtitle">Plataforma de análisis y visualización de datos para mejorar la producción de carne mediante modelos predictivos y control inteligente.</p>
+          <p className="project-tech">JavaScript + Python + Machine Learning + Computer Vision (EN DESARROLLO)</p>
 
 
         </div>
